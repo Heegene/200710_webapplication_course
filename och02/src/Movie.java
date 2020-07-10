14373bd51f1b5ee577766c312aa140dae9b84e71
Heegene <dev.heejin@gmail.com>
2020-07-10 16:46:55 +0900

    200710 Servlet exercise

--- och02/src/Movie.java
+++ och02/src/Movie.java
@@ -10,34 +10,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/GuGu") // 이거 주석처리하면 compiler가 찾을 수 없게 되어서 page not found 404 에러 
-public class GuGu extends HttpServlet {
+@WebServlet("/Movie")
+public class Movie extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     
-    public GuGu() {
+    public Movie() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	
+	// 여러개의 parameter를 한꺼번에 묶어서 보내는 경우의 서블릿 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int num = Integer.parseInt(request.getParameter("num")); // html에서 입력한 num으로부터 받아서 정수화
-		request.setCharacterEncoding("UTF-8");
+		// TODO Auto-generated method stub
 		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("UTF-8");
+		// 여러개의 값이므로 String 배열로 받음 
+		String[] movie = request.getParameterValues("movie"); // html에서 입력한 num으로부터 받아서 정수화
 		PrintWriter out = response.getWriter();
 		out.print("<html> <body>");
-		out.print("<h1> 구구단 " + num + " 단 </h1>");
-		
-		for (int i = 1; i < 10; i++) {
-			out.print(num + " * " + i + " = " + num*i + "<br>");
-			// 이렇게도 가능
-			// out.printf("%d * %d = %d", num*i, num, (num*i));
+		out.print("<h1> 당신이 좋아하는 영화는 </h1>" );
+		for (int i = 0; i < movie.length; i++) {
+			out.print(movie[i] + "<br>");
 		}
-		
 		out.print("</body> </html>");
 		out.close();
 	}
+		
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
